2f1d401ceb1a2869a049aa41fce4fe1bf50b7c2d
Mazen-Morsy <mazenmorsy707@gmail.com>
2025-10-26 14:13:15 +0300

    Student manager with save and load methods only

---
+++ StudentManagementSystem/src/model/StudentManager.java
@@ -0,0 +1,63 @@
+package model;
+
+import java.io.*;
+import java.util.*;
+
+public class StudentManager {
+    private List<Student>students=new ArrayList<>();
+    private String fileName;
+
+    StudentManager(String fileName){
+        this.fileName = fileName;
+        loadFromFile();
+    }
+
+    //file handling
+    private void saveToFile(){
+        try (BufferedWriter bw =new BufferedWriter(new FileWriter(fileName))) {
+            for ( Student s:students){
+                bw.write(s.toCSV());
+                bw.newLine();
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();   // hy print error
+        }
+    }
+
+    private  void loadFromFile(){
+        File file =new File(fileName);
+        if(!file.exists()){
+            return;    // get out
+        }
+
+        try(BufferedReader br =new BufferedReader(new FileReader(file)) ){ // hna ba3tna object mn el file msh el file name 5od balek
+            String line;
+            students.clear(); // 3ashan nbd2 3la ndafa
+
+            while((line= br.readLine())!=null){
+               String [] parts = line.split(",");
+               if(parts.length==6){
+                   int id=Integer.parseInt(parts[0]);
+                   String name=parts[1];
+                   int age=Integer.parseInt(parts[2]);
+                   String gender=parts[3];
+                   String department=parts[4];
+                   double gpa=Double.parseDouble(parts[5]);
+                   students.add(new Student(id,name,age,gender,department,gpa)); // keda 7atena fel arraylist el esmha students object gdeed mn el Student
+
+               }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
+
+
+
+
+
+
