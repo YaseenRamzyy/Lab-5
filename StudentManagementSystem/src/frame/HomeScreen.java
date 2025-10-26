27d140d960be7b6313006c0b542ac5807e01b865
Mazen-Morsy <mazenmorsy707@gmail.com>
2025-10-26 05:06:01 +0300

    Added home screen Gui only

--- StudentManagementSystem/src/frame/HomeScreen.java
+++ StudentManagementSystem/src/frame/HomeScreen.java
@@ -1,4 +1,17 @@
 package frame;
 
+import javax.swing.*;
+
 public class HomeScreen {
+    private JPanel Home;
+    private JButton addNewStudentButton;
+    private JButton displayAllRegisteredStudentsButton;
+    private JButton updateStudentDetailsButton;
+    private JButton deleteStudentRecordButton;
+    private JButton searchForAStudentButton;
+    private JLabel studentManagementSystemLabel;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
